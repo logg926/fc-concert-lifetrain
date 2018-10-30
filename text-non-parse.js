954653680825a7f734d094e6540e0fd181892b4e
logg926 <logg.com@gmail.com>
2018-10-30 12:05:12 +0800

    fix <

---
+++ text-non-parse.js
@@ -0,0 +1,74 @@
+<h1>`Porcia`</h1></br>
+Where r u, Lulu? `🧐``🧐`<br>
+<br>
+<h1>`Lulu`</h1>
+Next to u. Wt? `🤷🏻‍♀`<br>
+<br>
+<h1>`Porcia`</h1>
+Nth. Where r Log G and Wendy? `😛`<br>
+<br>
+<h1>`Log G`</h1>
+Porcia, Where r u sitting?`🧐``🧐 `<br>
+<br>
+<h1>`Lulu`</h1>
+Next to me. `😜`<br>
+<br>
+<h1>`Log G`</h1>
+Where r u? `🤔`<br>
+<br>
+<h1>`Wendy`</h1>
+Next to u, dude! `😅`<br>
+<br>
+<h1>`Porcia`</h1>
+U r next to me, wendy? <br>
+<br>
+<h1>`Wendy`</h1>
+I’m next to Lulu, where r u sitting ?`😓` <br>
+<br>
+<h1>`Kenny`</h1>
+Soooooooooo slpy,`💤``💤``💤``💤 `who’s next to me ? `🤔``🤔`<br>
+<br>
+<h1>`David`</h1>
+Am next to the door…. `😛`<br>
+<br>
+<h1>`Sherry`</h1>
+Huh? I’m at the door, who else near the door? `🤨``🤨`<br>
+<br>
+<h1>`Kenny`</h1>
+Where????????`🤯`<br>
+<br>
+<h1>`Lulu`</h1>
+So slpy… `💤``💤`Stay up until 2am to study arrrrr! `🤦🏻‍♀``🤦🏻‍♀`<br>
+<br>
+<h1>`Wendy`</h1>
+R u next to me? Hate staying up to study for tests … `😫``😫`<br>
+<br>
+<h1>`Kenny`</h1>
+In front of u, dude!`🙌🏻 `<br>
+<br>
+<h1>`Porcia`</h1>
+Hey! Can anyone help explain F=Ma `🤯``🤯``🤯`<br>
+<br>
+<h1>`Log G`</h1>
+Easy, man! `😏`<br>
+<br>
+<h1>`Sherry`</h1>
+R u here ? `🧐`<br>
+<br>
+<h1>`Log G`</h1>
+On the handrail lor`🤭`<br>
+<br>
+<h1>`Sherry`</h1>
+Which one? `🤔` <br>
+<br>
+<h1>`Porcia`</h1>
+Nvm, explain F=Ma la! `😵`<br>
+<br>
+<h1>`Wendy`</h1>
+Explain la X 2 `🤦🏻‍♀`<br>
+<br>
+<h1>`Kenny`</h1>
+Explain la X 3 `🙇🏻‍♂``🙌🏻`<br>
+<br>
+<h1>`Log G`</h1>
+U guys asking me ? `🤔``🤔``🤔``🤔``🤔`<br>
